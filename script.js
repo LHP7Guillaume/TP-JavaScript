7068ab4dc733e5f3dd10d8e8abf6632f03cb3844
Angeline <angelinecoignet@gmail.com>
2021-11-22 16:57:24 +0100

    Finalisation fichier json et début mise en page

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ let arrayShoes = data.results;
 
 for (i = 0; i < arrayShoes.length; i++) {
 
-    document.getElementById('shoes').innerHTML += `<div class="movie">
-    <img class="imgMovie" src="${arrayShoes[i].picture}" alt="">
-    <p class="title">${arrayShoes[i].original_name}</p>
-    <p class="summary">${arrayShoes[i].description} ...</p>
+    document.getElementById('allShoes').innerHTML += `<div class="shoes">
+    <img class="imgShoes" src="${arrayShoes[i].picture}" alt="">
+    <p class="nameShoes">${arrayShoes[i].original_name}</p>
+    <p class="category">${arrayShoes[i].category}</p>
+    <p class="descriptionShoes">${arrayShoes[i].description.slice(0, 50)}...</p>
 </div>`
 };
 })
