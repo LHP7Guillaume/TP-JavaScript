b86badf34c504790e91686b49e5f2ee44f4cf1c8
Angeline <angelinecoignet@gmail.com>
2021-11-23 15:47:32 +0100

    Affichage des artiles dans le panier au clic

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 //Afficher les articles selon la catégories choisies
-function displayShoes(filtre){
+function displayShoes(filtre) {
     fetch('shoes.json')
-    .then(response => response.json())
-    .then(data => {(data)
+        .then(response => response.json())
+        .then(data => {
+            (data)
 
-let arrayShoes = data.results;
-document.getElementById('allShoes').innerHTML ='';
-for (i = 0; i < arrayShoes.length; i++) {
-    if(filtre == 'All' || arrayShoes[i].category == filtre){
-        document.getElementById('allShoes').innerHTML += `<div class="shoes">
+            let arrayShoes = data.results;
+            document.getElementById('allShoes').innerHTML = '';
+            for (i = 0; i < arrayShoes.length; i++) {
+                if (filtre == 'All' || arrayShoes[i].category == filtre) {
+                    document.getElementById('allShoes').innerHTML += `<div class="shoes">
         <img class="imgShoes" src="${arrayShoes[i].picture}" alt="">
         <p class="nameShoes">${arrayShoes[i].original_name}</p>
         <p class="categoryShoes">${arrayShoes[i].category}</p>
         <p class="descriptionShoes">${arrayShoes[i].description.slice(0, 60)}...</p>
         <div class="gridAdd"><p class="priceShoes">${arrayShoes[i].price} €</p>
-        <button class="addShoes">Ajouter</button></div>
+        <button data-show data-name="${arrayShoes[i].original_name}" data-pict="${arrayShoes[i].picture}" data-ref="${arrayShoes[i].reference}" data-price="${arrayShoes[i].price}" class="addShoes">Ajouter</button></div>
         </div>`
-    }
-};
-})
+                }
+            };
+        })
 }
 
 displayShoes('All')
 document.addEventListener('click', displayFilter);
 
-function displayFilter(element){
-    if(element.target.className == 'choiceCat'){
+function displayFilter(element) {
+    if (element.target.className == 'choiceCat') {
         let catButton = element.target.textContent;
         displayShoes(catButton);
     }
@@ -43,7 +44,24 @@ document.getElementsByClassName('close')[0].onclick = () => {
 }
 
 
-function displayArticlesBasket(){
+
+
+function displayArticlesBasket(event) {
+    if (event.target.hasAttribute('data-show')){
+        let title = event.target.dataset.name;
+        let picture = event.target.dataset.pict;
+        let reference = event.target.dataset.ref;
+        let price = event.target.dataset.price;
+        
+        document.getElementById('gridModal').innerHTML +=    
+        `<img class="imgModal" src="${picture}" alt="">
+        <p class="nameModal">${title}</p>
+        <p class="refModal">Réf.: ${reference}</p>
+        <p class="qteModal">Qté</p>
+        <p class="priceModal">${price} €</p>
+        <div class="delete"><span class="iconify bin" data-icon="ri:delete-bin-5-line"></span></div>`  
+    }
     
 }
 
+document.addEventListener('click', displayArticlesBasket)
