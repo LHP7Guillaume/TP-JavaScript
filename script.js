5308fabb6f75d3a39c831975ac2ca2224034927a
Angeline <angelinecoignet@gmail.com>
2021-11-23 09:20:02 +0100

    Fonction js pour filtrer par catégorie + modification css

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-fetch('shoes.json')
+function displayShoes(filtre){
+    fetch('shoes.json')
     .then(response => response.json())
-    .then(data => {console.log(data)
+    .then(data => {(data)
 
 let arrayShoes = data.results;
-
+document.getElementById('allShoes').innerHTML ='';
 for (i = 0; i < arrayShoes.length; i++) {
-
-    document.getElementById('allShoes').innerHTML += `<div class="shoes">
-    <img class="imgShoes" src="${arrayShoes[i].picture}" alt="">
-    <p class="nameShoes">${arrayShoes[i].original_name}</p>
-    <p class="category">${arrayShoes[i].category}</p>
-    <p class="descriptionShoes">${arrayShoes[i].description.slice(0, 50)}...</p>
-</div>`
+    if(filtre == 'All' || arrayShoes[i].category == filtre){
+        document.getElementById('allShoes').innerHTML += `<div class="shoes">
+        <img class="imgShoes" src="${arrayShoes[i].picture}" alt="">
+        <p class="nameShoes">${arrayShoes[i].original_name}</p>
+        <p class="category">${arrayShoes[i].category}</p>
+        <p class="descriptionShoes">${arrayShoes[i].description.slice(0, 50)}...</p>
+        <p>${arrayShoes[i].price} €</p>
+        <button class="add">Ajouter au panier</button>
+        </div>`
+    }
 };
 })
+}
+
+displayShoes('All')
+document.addEventListener('click', displayFilter);
+
+function displayFilter(element){
+    if(element.target.className == 'choiceCat'){
+        let catButton = element.target.textContent;
+        displayShoes(catButton);
+    }
+}
