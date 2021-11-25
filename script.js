fbc380c22f5be444ad6ec84e0a81f3a65b275c34
Angeline <angelinecoignet@gmail.com>
2021-11-25 09:08:58 +0100

    Modification du total panier

--- script.js
+++ script.js
@@ -34,26 +34,27 @@ function displayFilter(element) {
 
 
 //Modifier la quantité et le prix
-function changeQte(valeur,index){
-    arrayRef[index][3]=valeur;
-    let xTot=arrayRef[index][3]*arrayRef[index][2];
-    let xNomId="price-"+index;
-    document.getElementById(xNomId).textContent=xTot+" €";
+function changeQte(valeur, index) {
+    arrayRef[index][3] = Number(valeur);
+    let xTot = arrayRef[index][3] * arrayRef[index][2];
+    document.getElementById("price-" + index).textContent = xTot + " €";
+    document.getElementById('totalBasket').textContent = `Total panier: ${calcTotal()} €`;
+    document.getElementById('countArt').textContent = countNbArt();
 }
 
 
 //Afficher le panier et son contenu
 document.getElementById('basket').onclick = () => {
-    document.getElementById('modalBasket').style.display = 'block';
-    document.getElementById('contentBasket').innerHTML = '';
-    let total = 0
-    for (let index in arrayRef) {
-        let title = arrayRef[index][0];
-        let picture = arrayRef[index][1];
-        let price = arrayRef[index][2];
-        let qte = arrayRef[index][3];
-        document.getElementById('contentBasket').innerHTML +=
-            `<div class="gridModal" id="obj-${index}">
+    if (countNbArt() != 0) {
+        document.getElementById('modalBasket').style.display = 'block';
+        document.getElementById('contentBasket').innerHTML = '';
+        for (let index in arrayRef) {
+            let title = arrayRef[index][0];
+            let picture = arrayRef[index][1];
+            let price = arrayRef[index][2];
+            let qte = arrayRef[index][3];
+            document.getElementById('contentBasket').innerHTML +=
+                `<div class="gridModal" id="obj-${index}">
              <img class="imgModal" src="${picture}" alt="">
              <p class="nameModal">${title}</p>
              <p class="refModal">Réf.: ${index}</p>
@@ -61,9 +62,9 @@ document.getElementById('basket').onclick = () => {
              <p id="price-${index}" class="priceModal">${price*qte} €</p>
              <img src="img/trash-alt-solid.svg" id="bin-${index}" class="delete"></img>
          </div>`;
-        total += price*qte;
+        }
+        document.getElementById('totalBasket').textContent = `Total panier: ${calcTotal()} €`;
     }
-    document.getElementById('totalBasket').textContent = `Total panier: ${total} €`;
 }
 
 //Fermer le panier
@@ -90,20 +91,42 @@ function addItem(event) {
         } else {
             arrayRef[xRef] = [title, picture, price, 1];
         }
+        document.getElementById('countArt').textContent = countNbArt();
     }
 }
 
 
 //Supprimer un article
-
 function deleteArticle(e) {
     if (e.target.id.includes('bin')) {
         let indexBin = e.target.id.split('-').pop();
         document.getElementById(`obj-${indexBin}`).remove();
         delete arrayRef[indexBin];
+        document.getElementById('totalBasket').textContent = `Total panier: ${calcTotal()} €`;
+        document.getElementById('countArt').textContent = countNbArt();
     }
     console.log(arrayRef)
 
 }
 
-document.addEventListener('click', deleteArticle)
+document.addEventListener('click', deleteArticle);
+
+//Calculer le total du panier
+function calcTotal() {
+    let total = 0;
+    for (let index in arrayRef) {
+        let price = arrayRef[index][2];
+        let qte = arrayRef[index][3];
+        total += price * qte;
+    }
+    return total;
+}
+
+//Afficher nombre d'articles dans le panier
+function countNbArt() {
+    let count = 0
+    for (let index in arrayRef) {
+        count += arrayRef[index][3];
+    }
+    return count;
+}
